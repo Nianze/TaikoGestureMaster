e1884724195f0014b329f0a834f5a47086db1cf6
zz284 <zz284@cornell.edu>
2015-12-03 22:11:53 -0500

    remove auto generated files from corner_detect and demo

--- xapp1167_vivado/sw/corner_detect/boot_image/bootgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bootgen -image bootimage.bif -o i BOOT.bin -w on
-if [ $? -eq 0 ]; then
-    echo "BOOT.bin successfully generated."
-else
-    echo "BOOT.bin failed to generate."
-fi
